229e7e6ae6b4225e4adeeb909b008dcae61349da
Pedro Torres <20152863@itla.edu.do>
2017-06-01 12:44:00 -0400

    Primera Insercion

---
+++ Calculadora de prestamos/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Calculadora_de_prestamos
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Box1 = new System.Windows.Forms.TextBox();
+            this.Box3 = new System.Windows.Forms.TextBox();
+            this.Box2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tabla = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto en RD$:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tasa de interes %:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Plazo en meses:";
+            // 
+            // Box1
+            // 
+            this.Box1.Location = new System.Drawing.Point(157, 12);
+            this.Box1.Margin = new System.Windows.Forms.Padding(2);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(158, 20);
+            this.Box1.TabIndex = 3;
+            this.Box1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.Box1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box1_KeyPress);
+            // 
+            // Box3
+            // 
+            this.Box3.Location = new System.Drawing.Point(157, 85);
+            this.Box3.Margin = new System.Windows.Forms.Padding(2);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(158, 20);
+            this.Box3.TabIndex = 4;
+            this.Box3.TextChanged += new System.EventHandler(this.Box3_TextChanged);
+            this.Box3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box3_KeyPress);
+            // 
+            // Box2
+            // 
+            this.Box2.Location = new System.Drawing.Point(157, 49);
+            this.Box2.Margin = new System.Windows.Forms.Padding(2);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(158, 20);
+            this.Box2.TabIndex = 5;
+            this.Box2.TextChanged += new System.EventHandler(this.Box2_TextChanged);
+            this.Box2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Box2_KeyPress);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.White;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCalcular.FlatAppearance.BorderSize = 3;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(356, 61);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(91, 41);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 3;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(356, 122);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(91, 41);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Limpiar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Box1);
+            this.panel1.Controls.Add(this.Box3);
+            this.panel1.Controls.Add(this.Box2);
+            this.panel1.Location = new System.Drawing.Point(7, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 124);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(11, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(321, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Calculadora de Prestamos";
+            // 
+            // Tabla
+            // 
+            this.Tabla.AllowUserToAddRows = false;
+            this.Tabla.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tabla.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.Tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Tabla.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column6,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.Tabla.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Tabla.Location = new System.Drawing.Point(0, 216);
+            this.Tabla.Name = "Tabla";
+            this.Tabla.ReadOnly = true;
+            this.Tabla.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Tabla.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Tabla.Size = new System.Drawing.Size(723, 267);
+            this.Tabla.TabIndex = 12;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Pago ";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Cuota";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Intereses";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Amortizacion principal";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 135;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Amoritizacion acumulado";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 145;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Capital";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(723, 483);
+            this.Controls.Add(this.Tabla);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Calculadora_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabla)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Box1;
+        private System.Windows.Forms.TextBox Box3;
+        private System.Windows.Forms.TextBox Box2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView Tabla;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ Calculadora de prestamos/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_prestamos
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        double Monto;
+        double Interes;
+        int Meses;
+        double result;
+         
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Box2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Box3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Box1_KeyPress(object sender, KeyPressEventArgs e) //Validar solo entra de numeros en los TexBox
+        {
+            Input(e);
+        }
+
+        public  KeyPressEventArgs Input(KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else if (Char.IsSymbol(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Introducir solo numeros", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            return e; 
+        }
+
+        private void Box2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Input(e);
+        }
+
+        private void Box3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Input(e);
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e) //Calcular Prestamos
+        {
+            double cuota;
+            double intereses;
+            double capital = Convert.ToDouble(Box1.Text);
+            double tasa = Convert.ToDouble(Box2.Text);
+            double plazo = Convert.ToDouble(Box3.Text);
+            double amort;
+            double amortAcumulado = 0;
+
+            tasa /= 1200;
+            cuota = capital * (tasa / (double)(1 - Math.Pow(1 + (double)tasa, - plazo)));
+            cuota = Math.Round(cuota, 2);
+            for (int i = 0; i <= plazo; i++)
+            {
+                if (i == 0)
+                {
+                    
+                }
+                else
+                {
+                    intereses = Math.Round((capital * tasa), 2);
+                    amort = Math.Round((cuota - intereses), 2);
+                    amortAcumulado += Math.Round(amort, 2);
+                    capital -= Math.Round(amort, 2);
+                    if(capital < 0)
+                    {
+                        
+                        capital = (capital * -1);
+                        
+                    }
+                    Tabla.Rows.Add(i, cuota, intereses, amort, amortAcumulado, capital);
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e) //Limpiar TexBox
+        {
+            Box1.Text = " ";
+            Box2.Text = " ";
+            Box3.Text = " ";
+            Tabla.Rows.Clear();
+        }
+    }
+}
